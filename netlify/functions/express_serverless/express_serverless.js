31434350fd8807221cabddf9d33f64ccc8c37a83
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 15:37:51 +0530

    add mongoDb

--- netlify/functions/express_serverless/express_serverless.js
+++ netlify/functions/express_serverless/express_serverless.js
@@ -1,10 +1,22 @@
 const express=require("express");
 const serverless=require("serverless-http");
+const mongoose=require("mongoose");
 const app=express();
-const path=require("path")
+const path=require("path");
+const dbConnection=require(path.join(__dirname,"..", "..", "..", "packages", "config", "dbConnection"));
+
+let isDBconnected=false;
+dbConnection();
 
 app.use('/login',require("../../../packages/routers/login") );
 app.use('/signUp',require(path.join(__dirname,"..", "..", "..", "packages", "routers", "signUp")) );
 
 
-module.exports.handler=serverless(app);
+module.exports.handler = serverless(async (event, context) => {
+  console.log("connected to server")
+  mongoose.connection.once('open', ()=>{
+    console.log("connected to DB");
+  })
+  const result = await app(event, context);
+  return result;
+});
