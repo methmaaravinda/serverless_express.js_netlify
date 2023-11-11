0a0a9ed531e0320a93007c1e414989c34fd08113
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 17:26:30 +0530

    add new function

--- packages/config/dbConnection.js
+++ packages/config/dbConnection.js
@@ -1,13 +1,15 @@
 const mongoose=require("mongoose");
 require('dotenv').config();
 
-const dbConnection=async()=>{
+const dbConnection=async(req, res)=>{
     try{
-        mongoose.connect(
+        await mongoose.connect(
             process.env.MONGO_DB_URI,
             { useNewUrlParser: true, useUnifiedTopology: true }
         );
+        res.json({message: "success to connected with db"})
     }catch(err){
+        res.json({error: "fail to connect db !"})
         console.error(err);
     }
 }
--- packages/routers/signUp.js
+++ packages/routers/signUp.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const router=express.Router();
+const dbCoonection=require("../../packages/config/dbConnection")
 
 router.route("/")
-    .get((req, res)=>{
-        console.log( process.env.MONGO_DB_URI)
-        res.json({message:"signUp router"+ process.env.MONGO_DB_URI})
+    .get(async(req, res)=>{
+        // console.log( process.env.MONGO_DB_URI)
+        // res.json({message:"signUp router1"+ process.env.MONGO_DB_URI})
+        dbCoonection(req, res);
     })
 
 module.exports=router;
