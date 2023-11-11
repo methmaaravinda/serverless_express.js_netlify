5e01bdd5932ab99a20b66743e6d7d70f13396909
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 22:44:18 +0530

    add a address to  whitelist

--- packages/config/corsOptions.js
+++ packages/config/corsOptions.js
@@ -1,4 +1,8 @@
-const whiteList=["http://localhost:3001", "http://192.168.8.119:3001"];
+const whiteList=[
+    "http://localhost:3001", 
+    "http://192.168.8.119:3001", 
+    "https://startling-cactus-bb2189.netlify.app"
+];
 const corsOptions={
     origin:( origin, callBack )=>{
         if(whiteList.indexOf(origin)!=-1){
