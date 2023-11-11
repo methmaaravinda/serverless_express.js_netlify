b6450ef30d77bc6abe28e870d813f586871c3c9c
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 12:41:04 +0530

    first commit

---
+++ netlify/functions/express_serverless/express_serverless.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const serverless=require("serverless-http");
+
+const app=express();
+
+const router=express.Router();
+app.get("/", (req, res)=>{
+  res.json({suceess: "from server!"})
+})
+router.get('/',(req, res)=>{
+    console.log(req.method);
+    res.json({message:"get request to serverless !"})
+})
+
+app.use('/.netlify/functions/express_serverless', router)
+
+module.exports.handler=serverless(app);
