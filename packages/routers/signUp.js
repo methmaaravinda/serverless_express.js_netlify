174bb404f446f3c0a3d20171755600d09cf8b80d
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 22:57:09 +0530

    add signUp router

--- packages/routers/signUp.js
+++ packages/routers/signUp.js
@@ -3,15 +3,45 @@ const router=express.Router();
 const User=require("../model/UserModel");
 
 
-router.route("/")
-    .get(async(req, res)=>{
-        const user={email: "testing 01", password:"123"};
-        try{
-            await User.create(user);
-            res.json({user});
-        }catch(err){
-            res.json({err})
-        }
+router.route('/')
+    .post((req, res)=>{
+        const email=req.body?.user?.email;
+        const password=req.body?.user?.password;
+        const user={email,password};
+        if(email && password){
+            (async()=>{
+                try{
+                    await User.create(user);
+                    res.json(user);
+                }catch(err){
+                    res.json(err)
+                }
+            })()
+        }else return res.json({error : "no user object !"})
     })
-
 module.exports=router;
+
+// const express=require("express");
+// const router=express.Router();
+// const User=require('../model/userModel')
+
+// router.route('/')
+//     .post((req, res)=>{
+//         const email=req.body?.user?.email;
+//         const password=req.body?.user?.password;
+//         const user={email,password};
+//         if(email && password){
+//             (async()=>{
+//                 try{
+//                     await User.create(user);
+//                     res.json(user);
+//                 }catch(err){
+//                     res.json(err)
+//                 }
+//             })()
+//         }else return res.json({error : "no user object !"})
+//     })
+   
+
+
+// module.exports=router;
