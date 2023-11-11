86acabad173d38a51d4433bb5a8d2b56390b88d7
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 17:18:25 +0530

    add value

--- packages/routers/signUp.js
+++ packages/routers/signUp.js
@@ -4,7 +4,7 @@ const router=express.Router();
 router.route("/")
     .get((req, res)=>{
         console.log( process.env.MONGO_DB_URI)
-        res.json({message:"signUp router"})
+        res.json({message:"signUp router"+ process.env.MONGO_DB_URI})
     })
 
 module.exports=router;
