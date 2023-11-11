d764bde1215b6697e11f3df8cffa8c2aa28e18e5
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-11 18:13:12 +0530

    new test

--- packages/config/dbConnection.js
+++ packages/config/dbConnection.js
@@ -1,17 +1,33 @@
+// const mongoose=require("mongoose");
+// require('dotenv').config();
+
+// const dbConnection=async(req, res)=>{
+//     try{
+//         await mongoose.connect(
+//             process.env.MONGO_DB_URI,
+//             { useNewUrlParser: true, useUnifiedTopology: true }
+//         );
+//         const db = mongoose.connection.db;
+//         const dbName = await db.databaseName;
+//         res.json({message: "success to connected with db"+dbName})
+//     }catch(err){
+//         res.json({error: "fail to connect db !"})
+//         console.error(err);
+//     }
+// }
+
+// module.exports=dbConnection;
+
 const mongoose=require("mongoose");
 require('dotenv').config();
 
-const dbConnection=async(req, res)=>{
+const dbConnection=async()=>{
     try{
-        await mongoose.connect(
+        mongoose.connect(
             process.env.MONGO_DB_URI,
             { useNewUrlParser: true, useUnifiedTopology: true }
         );
-        const db = mongoose.connection.db;
-        const dbName = await db.databaseName;
-        res.json({message: "success to connected with db"+dbName})
     }catch(err){
-        res.json({error: "fail to connect db !"})
         console.error(err);
     }
 }
--- packages/routers/signUp.js
+++ packages/routers/signUp.js
@@ -1,12 +1,9 @@
 const express=require("express");
 const router=express.Router();
-const dbCoonection=require("../../packages/config/dbConnection")
 
 router.route("/")
     .get(async(req, res)=>{
-        // console.log( process.env.MONGO_DB_URI)
-        // res.json({message:"signUp router1"+ process.env.MONGO_DB_URI})
-        dbCoonection(req, res);
+        res.json({success: "success Login !"})
     })
 
 module.exports=router;
