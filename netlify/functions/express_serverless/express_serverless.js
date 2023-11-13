4a9ef8d903e38cb2813d47567034d2683a22559d
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-13 13:40:00 +0530

    add users route

--- netlify/functions/express_serverless/express_serverless.js
+++ netlify/functions/express_serverless/express_serverless.js
@@ -15,6 +15,7 @@ app.use('/login',require("../../../packages/routers/login") );
 app.use('/signUp',require("../../../packages/routers/signUp") );
 app.use('/refreshToken', require("../../../packages/routers/refreshToken") );
 app.use(require("../../../packages/middlewares/verify"));
+app.use('/users', require("../../../packages/routers/users"));
 
 module.exports.handler = serverless(async (event, context) => {
   console.log("connected to server")
