2761aee9ed0708ca627105cd2529f75408d0c29c
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-15 17:39:39 +0530

    add user route

--- packages/routers/users.js
+++ packages/routers/users.js
@@ -1,18 +1,21 @@
-const express=require("express");
-const router=express.Router();
-const Profile=require("../model/ProfileModel");
+const express = require("express");
+const router = express.Router();
+const Profile = require("../model/ProfileModel");
 
 router.route("/")
-    .post(async(req, res)=>{
-        const id=Number(req?.params?.id);
-        if(String(id)==="NaN") return res.json({error: "params not valid ! "})
-        try{
-            const profiles=await Profile.find().sort({ createdAt: -1 }).limit(6*id);
-            res.json({profiles});
-        }catch(err){
-            res.status(500).json({error_msg: "error finding profiles !", error: err});
-        }
-    })
+  .post(async (req, res) => {
+    const id = Number(req.params.id);
 
-module.exports=router;
+    if (isNaN(id)) {
+      return res.json({ error: "params not valid!" });
+    }
 
+    try {
+      const profiles = await Profile.find().sort({ createdAt: -1 }).limit(6 * id);
+      res.json({ profiles });
+    } catch (err) {
+      res.status(500).json({ error_msg: "error finding profiles!", error: err });
+    }
+  });
+
+module.exports = router;
